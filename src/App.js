995e647cc5013dfcd410434ed1fb9164e857d2d4
Kseniya Valadzko <kseniya.valadzko@itibo.com>
2020-10-15 19:51:13 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Welcome to valadzkokseniya.com!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
