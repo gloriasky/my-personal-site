7f35768afb4ebc3c504c035869cf7d152b4b05d8
Valadzko Kseniya <volodko.kj@gmail.com>
2020-10-17 13:09:28 +0300

    replace functional component with a class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Welcome to valadzkokseniya.com!</h3>
-          <h5>For contact use <a>valadzkokseniya@gmail.com</a></h5>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h3>Welcome to valadzkokseniya.com!</h3>
+                    <h5>For contact use <a href=''>valadzkokseniya@gmail.com</a></h5>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
