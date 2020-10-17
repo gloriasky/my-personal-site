0b4afe8973af134aadf13586b2e78ab0fd61201b
Valadzko Kseniya <volodko.kj@gmail.com>
2020-10-17 12:00:36 +0300

    Add contact message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>Welcome to valadzkokseniya.com!</h3>
+          <h5>For contact use <a>valadzkokseniya@gmail.com</a></h5>
       </header>
     </div>
   );
